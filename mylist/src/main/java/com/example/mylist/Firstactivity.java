78b84a84592da92b200df9f375b8744050792bf5
陈征 <570806807@qq.com>
2015-07-23 15:19:19 +0800

    添加日志头

--- mylist/src/main/java/com/example/mylist/Firstactivity.java
+++ mylist/src/main/java/com/example/mylist/Firstactivity.java
@@ -5,6 +5,7 @@ import android.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -13,6 +14,7 @@ public class Firstactivity extends AppCompatActivity implements ItemFragment.OnF
 
     public static  String  lf="listfragment";
 
+    public static String   TAG="TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
